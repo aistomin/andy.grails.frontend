9a4c1531ae65cb1bfdadc9eae43281b1d3023b38
aistomin <andrej.istomin.ikeen@gmail.com>
2025-05-28 00:43:46 +0200

    Issue #27. Search function is fixed.

--- src/app/video.ts
+++ src/app/video.ts
@@ -1,15 +1,14 @@
 export interface Video {
-    id: number;
-    name: string;
-    description: string;
-    youtubeId: string;
-    createdAt: string;
-    publishedAt: string;
-    city: string;
-    state: string;
-    photo: string;
-    availableUnits: number;
-    wifi: boolean;
-    laundry: boolean;
-  }
-  
+  id: number;
+  title: string;
+  description: string;
+  youtubeId: string;
+  createdAt: string;
+  publishedAt: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
