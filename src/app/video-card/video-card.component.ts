807a8057791b9ba56d0e97905ef5bcb119de9d9c
aistomin <andrej.istomin.ikeen@gmail.com>
2025-05-29 05:03:51 +0200

    Issue #51. Angular dependencies are updated. video-card is fixed.

--- src/app/video-card/video-card.component.ts
+++ src/app/video-card/video-card.component.ts
@@ -9,34 +9,39 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   imports: [CommonModule, RouterModule],
   template: `
     <section class="video-card">
-      <iframe [src]="getYouTubeUrl()" 
+      <iframe
+        [src]="getYouTubeUrl()"
         class="video-card-youtube-video"
-        frameborder="0" 
+        frameborder="0"
         allow="accelerometer; 
         autoplay; 
         clipboard-write; 
         encrypted-media; 
         gyroscope; 
         picture-in-picture; 
-        web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
+        web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      >
       </iframe>
-      <h2 class="video-card-heading">{{ video.name }}</h2>
-      <p class="video-card-description">{{ truncateDescription(video.description) }}</p>
+      <h2 class="video-card-heading">{{ video.title }}</h2>
+      <p class="video-card-description">
+        {{ truncateDescription(video.description) }}
+      </p>
       <a [routerLink]="['/details', video.id]">More ...</a>
     </section>
   `,
-  styleUrls: ['./video-card.component.scss']
+  styleUrls: ['./video-card.component.scss'],
 })
 export class VideoCardComponent {
-
   @Input() video!: Video;
 
   constructor(private sanitizer: DomSanitizer) {}
 
   getYouTubeUrl(): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.video.youtubeId}`);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube.com/embed/${this.video.youtubeId}`
+    );
   }
 
   truncateDescription(description: string): string {
