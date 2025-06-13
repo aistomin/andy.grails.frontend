2ef09bcd0435f7d850d5e95a7a1d26ba7b5528f8
aistomin <andrej.istomin.ikeen@gmail.com>
2025-06-13 20:03:46 +0200

    Issue #12. The footer is added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import {Component} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   template: `
-   <main>
+    <main>
       <a [routerLink]="['/']">
         <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/>
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
         </header>
       </a>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
+      <footer class="footer">
+        <p>&copy; 2025 Andy Grails. All rights reserved.</p>
+      </footer>
     </main>
   `,
   styleUrls: ['./app.component.scss'],
