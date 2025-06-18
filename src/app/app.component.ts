f4965888cd8f5fcc61cb2724e4f75cf8facd4415
aistomin <andrej.istomin.ikeen@gmail.com>
2025-06-18 03:10:10 +0200

    Issue #77. Footer is extracted to the separated component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FooterComponent],
   template: `
     <main>
       <a [routerLink]="['/']">
@@ -20,46 +21,7 @@ import { CommonModule } from '@angular/common';
       <section class="content">
         <router-outlet></router-outlet>
       </section>
-      <footer class="footer">
-        <div class="footer-content">
-          <div class="footer-left">
-            <a href="/privacy">Privacy</a>
-            <span class="separator">|</span>
-            <a href="/imprint">Imprint/Terms</a>
-            <span class="separator">|</span>
-            <a href="https://github.com/aistomin">Developed by me</a>
-          </div>
-          <div class="footer-center">
-            <p>&copy; 2025 Andy Grails</p>
-          </div>
-          <div class="footer-right">
-            <a
-              href="https://youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="YouTube"
-            >
-              <i class="fab fa-youtube"></i>
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Instagram"
-            >
-              <i class="fab fa-instagram"></i>
-            </a>
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Facebook"
-            >
-              <i class="fab fa-facebook"></i>
-            </a>
-          </div>
-        </div>
-      </footer>
+      <app-footer></app-footer>
     </main>
   `,
   styleUrls: ['./app.component.scss'],
